11bb4436c53b61ca1b603596d9228b1c653ece91
Euan Fullarton <euanfullarton7@gmail.com>
2017-04-20 15:53:48 +0100

    4 initial tests in BetTest passing, however resolvedOn is currently a String when it should really be a Date

--- Wagr/src/main/java/example/codeclan/com/wagr/Bet.java
+++ Wagr/src/main/java/example/codeclan/com/wagr/Bet.java
@@ -1,8 +1,44 @@
 package example.codeclan.com.wagr;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * Created by user on 20/04/2017.
  */
 
 public class Bet {
+
+    private String category;
+    private String details;
+    private String resolvedOn;
+    private int stake;
+
+    public Bet (String category, String details, String resolvedOn, Integer stake){
+        this.category = category;
+        this.details = details;
+        this.resolvedOn = resolvedOn;
+        this.stake = stake;
+    }
+
+    public String getCategory(){
+        return category;
+    }
+
+    public String getDetails(){
+        return details;
+    }
+
+    public String getResolvedOn(){
+        return resolvedOn;
+    }
+
+    public Integer getStake(){
+        return stake;
+    }
+
+
+
 }
--- Wagr/src/test/java/example/codeclan/com/wagr/BetTest.java
+++ Wagr/src/test/java/example/codeclan/com/wagr/BetTest.java
@@ -1,8 +1,44 @@
 package example.codeclan.com.wagr;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by user on 20/04/2017.
  */
 
 public class BetTest {
+
+    Bet bet;
+
+    @Before
+    public void before(){
+
+        bet = new Bet("Football", "Arsenal to win league", "31st May 2017", 25);
+//        will come back to this at a later point, date currently a string
+    }
+
+    @Test
+    public void hasCategory(){
+        assertEquals("Football", bet.getCategory());
+    }
+
+    @Test
+    public void hasDetails(){
+        assertEquals("Arsenal to win league", bet.getDetails());
+    }
+
+    @Test
+    public void hasResolvedOn(){
+        assertEquals("31st May 2017", bet.getResolvedOn());
+    }
+
+    @Test
+    public void hasStake(){
+        assertEquals(25, (int)bet.getStake());
+    }
+
+
 }
